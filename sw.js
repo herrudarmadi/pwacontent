8ef3573b5df360c698ba360928330f0efa0ced31
herrudarmadi <137267873+herrudarmadi@users.noreply.github.com>
2024-08-01 11:58:48 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@ const VERSION = "v0.0.0";
 // The name of the cache
 const CACHE_NAME = `pwacontent-${VERSION}`;
 
-const DIR = '/tespwa';
+const DIR = '/pwacontent';
 
 // The static resources that the app needs to function.
 const APP_STATIC_RESOURCES = [
@@ -44,11 +44,6 @@ self.addEventListener("activate", (event) => {
 // On fetch, intercept server requests
 // and respond with cached responses instead of going to network
 self.addEventListener("fetch", (event) => {
-  // As a single page app, direct app to always go to cached home page.
-  // if (event.request.mode === "navigate") {
-  //   event.respondWith(caches.match("/"));
-  //   return;
-  // }
 
   // For all other requests, go to the cache first, and then the network.
   event.respondWith(
@@ -60,8 +55,35 @@ self.addEventListener("fetch", (event) => {
         // Return the cached response if it's available.
         return cachedResponse;
       }
+
+      // IMPORTANT: Clone the request. A request is a stream and
+      // can only be consumed once. Since we are consuming this
+      // once by cache and once by the browser for fetch, we need
+      // to clone the request.
+      var fetchRequest = event.request.clone();
+      return fetch(fetchRequest).then(
+                function(response) {
+                    // Check if we received a valid response
+                    if (!response || response.status !== 200 || response.type !== 'basic') {
+                        return response;
+                    }
+
+                    // IMPORTANT: Clone the response. A response is a stream
+                    // and because we want the browser to consume the response
+                    // as well as the cache consuming the response, we need
+                    // to clone it so we have two streams.
+                    var responseToCache = response.clone();
+
+                    caches.open('dynamic-v1').then(function(cache) {
+                        cache.put(event.request, responseToCache);
+                    });
+
+                    return response;
+                }
+            );
+      
       // If resource isn't in the cache, return a 404.
-      return new Response(null, { status: 404 });
+      // return new Response(null, { status: 404 });
     })(),
   );
 });
