5959932d70dd80d5a344e9a7e779c4d698e60baf
herrudarmadi <137267873+herrudarmadi@users.noreply.github.com>
2024-08-01 10:50:01 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
+// Check if the browser is online or offline
+window.addEventListener('offline', () => {
+    // Show the offline message
+    document.getElementById('offlineMessage').style.display = 'block';
+});
 
+window.addEventListener('online', () => {
+    // if (isSending) {
+    //   registerSync();
+    // }
+    // Hide the offline message
+    document.getElementById('offlineMessage').style.display = 'none';
+});
+
+window.addEventListener('load', function() {
+    document.getElementById('offlineMessage').style.display = (navigator.onLine ? 'none' : 'block');
+});
